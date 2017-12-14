4ff5fdc43576f13419f74074d9ddcad865c1aabd
Yusuf Christian <yusufchristian@mhs.unesa.ac.id>
2017-12-14 16:33:43 +0700

    Database progress

---
+++ app/src/main/java/com/creativate/app/penaku/Constants.java
@@ -0,0 +1,11 @@
+package com.creativate.app.penaku;
+
+/**
+ * Created by Yusuf on 13/12/2017.
+ */
+
+public class Constants {
+    private static final String ROOT_URL = "https://192.168.12.19/Android/v1/";
+
+    public static final String URL_REGISTER = "registerUser.php";
+}
--- app/src/main/java/com/creativate/app/penaku/RegisterActivity.java
+++ app/src/main/java/com/creativate/app/penaku/RegisterActivity.java
@@ -1,13 +1,97 @@
 package com.creativate.app.penaku;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextUsername, editTextPassword, editTextEmail;
+    private Button buttonRegister;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editTextUsername = (EditText) findViewById(R.id.reg_username);
+        editTextEmail = (EditText) findViewById(R.id.reg_email);
+        editTextPassword = (EditText) findViewById(R.id.reg_password);
+
+        buttonRegister = (Button) findViewById(R.id.btnRegister);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonRegister.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        final String email = editTextEmail.getText().toString().trim();
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        progressDialog.setMessage("Mendaftarkan...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG);
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonRegister){
+            registerUser();
+        }
     }
 }
